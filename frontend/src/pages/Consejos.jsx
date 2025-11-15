61a10067e0984eab1ce28acd8df5ef2b9464ede4
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:43:47 -0500

    feat: creacion de la pagina consejos

---
+++ frontend/src/pages/Consejos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSmart from '../components/ui/NavbarSmart';
+
+export default function Consejos() {
+    return (
+        <>
+        <NavbarSmart />
+        <div className="page-container">
+            <h1 className="page-title">Consejos</h1>
+            {/* Tu contenido aquí */}
+        </div>
+        </>
+    );
+}
