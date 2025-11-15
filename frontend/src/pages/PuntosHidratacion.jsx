c1e54f0bb7bde530e944b9dc2885235b6054165f
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:46:35 -0500

    feat: creacion pagina para los puntos de hidratacion

---
+++ frontend/src/pages/PuntosHidratacion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSmart from '../components/ui/NavbarSmart';
+
+export default function PuntosHidratacion() {
+    return (
+        <>
+        <NavbarSmart />
+        <div className="page-container">
+            <h1 className="page-title">Puntos de Hidratación</h1>
+            {/* Tu contenido aquí */}
+        </div>
+        </>
+    );
+}
